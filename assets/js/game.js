ea7ae42910473a383f8ab170901f280a7fe8e734
Quntarious <90361495+Quntarious@users.noreply.github.com>
2022-01-14 17:19:21 -0600

    start and end function

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,6 +71,13 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 }
 };
  
+// function to start a new game
+var startGame = function() { 
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
 for (var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
@@ -92,7 +99,34 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert("You have lost your robot in battle! Game Over!");
     break;
   }
+   // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+   endGame();
 }
+};
+
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+
+    // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+  }
+}
+
+// start the game when the page loads
+startGame();
 
 
 
